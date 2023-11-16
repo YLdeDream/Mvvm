aaa2bb5757fa4969e86a3462f9de008ecbf14aef
YuLiang11 <1336739694@qq.com>
2023-11-16 16:21:13 +0800

    1.0.4

--- app/src/main/java/com/shangzuo/rxhttp/MainActivity.kt
+++ app/src/main/java/com/shangzuo/rxhttp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.shangzuo.rxhttp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.blankj.utilcode.util.AppUtils
 import com.shangzuo.mvvm.BaseUtils
 
 
@@ -10,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AppUtils.getAppVersionName()
+       // AppUtils.getAppVersionName()
 
         BaseUtils.getTest()
     }
--- mvvm/src/main/java/com/shangzuo/mvvm/BaseUtils.java
+++ mvvm/src/main/java/com/shangzuo/mvvm/BaseUtils.java
@@ -2,8 +2,11 @@ package com.shangzuo.mvvm;
 
 import android.util.Log;
 
+import com.blankj.utilcode.util.AppUtils;
+
 public class BaseUtils {
     public static void getTest(){
+        AppUtils.getAppVersionName();
         Log.e("TAG", "getTest: ==测试成功sss==" );
     }
 
