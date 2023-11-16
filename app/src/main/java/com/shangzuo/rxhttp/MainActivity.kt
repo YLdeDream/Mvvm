964f5fb574fb7e0f9937bdd6a8cfd288b24e3710
YuLiang11 <1336739694@qq.com>
2023-11-16 15:47:36 +0800

    1.0.2

--- app/src/main/java/com/shangzuo/rxhttp/MainActivity.kt
+++ app/src/main/java/com/shangzuo/rxhttp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.shangzuo.rxhttp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.blankj.utilcode.util.AppUtils
 import com.shangzuo.mvvm.BaseUtils
 
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppUtils.getAppVersionName()
 
         BaseUtils.getTest()
     }
