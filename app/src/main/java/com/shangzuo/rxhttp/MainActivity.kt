4b31120bd34a5b523d5795e1cc303d943d392b2c
YuLiang11 <1336739694@qq.com>
2023-11-16 14:58:08 +0800

    Initial commit

---
+++ app/src/main/java/com/shangzuo/rxhttp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.shangzuo.rxhttp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shangzuo.mvvm.BaseUtils
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        BaseUtils.getTest()
+    }
+}
---
+++ mvvm/src/main/java/com/shangzuo/mvvm/BaseUtils.java
@@ -0,0 +1,10 @@
+package com.shangzuo.mvvm;
+
+import android.util.Log;
+
+public class BaseUtils {
+    public static void getTest(){
+        Log.e("TAG", "getTest: ==测试成功==" );
+    }
+
+}
