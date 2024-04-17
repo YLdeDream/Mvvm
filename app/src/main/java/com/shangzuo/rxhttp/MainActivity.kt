e362cded80da0cecdbd548263430c3bc67a3727f
YuLiang11 <1336739694@qq.com>
2024-04-17 10:52:56 +0800

    1.1.6

--- app/src/main/java/com/shangzuo/rxhttp/MainActivity.kt
+++ app/src/main/java/com/shangzuo/rxhttp/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
        // AppUtils.getAppVersionName()
+
+
         AppUtils.getAppVersionName()
 
         BaseUtils.getTest()
